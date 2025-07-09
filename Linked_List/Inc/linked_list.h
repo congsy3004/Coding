f2dd75b42a8866326be382c8bbc10877b3dad4f4
congsy3004 <congsyhuynh3004@gmail.com>
2025-07-09 10:49:19 +0700

    Init repo

---
+++ Linked_List/Inc/linked_list.h
@@ -0,0 +1,61 @@
+/******************************************************************************
+ * @file           : linked_list.h
+ * @brief          : Brief description
+ * @author         : Your Name
+ * @date           : 09-Jul-2025
+ ******************************************************************************/
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* -------------------------------------------------------------------------
+ *                           << Include Files >>                                   
+ * -------------------------------------------------------------------------*/
+#include "llist_type.h"
+#include <stdint.h>
+
+/* -------------------------------------------------------------------------
+ *                           << Exported Types >>                                   
+ * -------------------------------------------------------------------------*/
+typedef enum llist_retval
+{
+	LLIST_RET_OK, // return OK value
+	LLIST_RET_NOK, // return OK value
+	LLIST_RET_TRUE, // return OK value
+	LLIST_RET_FALSE, // return OK value
+};
+/* -------------------------------------------------------------------------
+ *                           << Exported Constants >>                                   
+ * -------------------------------------------------------------------------*/
+
+/* -------------------------------------------------------------------------
+ *                           << Exported Macros >>                                   
+ * -------------------------------------------------------------------------*/
+
+/* -------------------------------------------------------------------------
+ *                           << Exported Functions >>                                   
+ * -------------------------------------------------------------------------*/
+llist_desc_t* llist_create( uint32_t size );
+llist_retval_t llist_destroy( llist_desc_t* list );
+llist_retval_t llist_init( llist_desc_t* list );
+llist_retval_t llist_is_empty( llist_desc_t* list );
+uint32_t	  llist_get_size( llist_desc_t* list );
+llist_retval_t llist_ins_head( llist_desc_t* list );
+llist_retval_t llist_ins_tail( llist_desc_t* list );
+llist_retval_t llist_pop_head( llist_desc_t* list );
+llist_retval_t llist_pop_tail( llist_desc_t* list );
+
+llist_retval_t llist_ins_after(llist_node_t* node, void* data );
+llist_retval_t llist_find(llist_desc_t* list, void* dest, void* find );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LINKED_LIST_H */
+
+/******************************** END OF FILE ********************************/
---
+++ Linked_List/Inc/llist_type.h
@@ -0,0 +1,12 @@
+#ifndef LLIST_TYPE_H
+#define LLIST_TYPE_H
+
+typedef struct llist_node llist_node_t;
+typedef struct llist_desc llist_desc_t;
+typedef struct  llist_handler llist_handler_t;
+
+typedef enum llist_retval llist_retval_t;
+
+
+
+#endif
